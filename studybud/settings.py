bf5c8ea2adaac39b6bfced7a9cf263b66f4b8783
Francis-Mwaniki <francismwaniki630@gmail.com>
2022-12-18 18:16:54 +0300

    new

--- studybud/settings.py
+++ studybud/settings.py
@@ -134,6 +134,8 @@ STATICFILES_DIRS =[
     BASE_DIR / 'static'
 ]
 #STATIC_ROOT =[]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 # Default primary key field type
